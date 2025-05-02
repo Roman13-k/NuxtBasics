584bf912829b266d49e2b01493006b78adab8089
Roman13-k <romank050607@gmail.com>
2025-05-02 11:29:11 +0300

    init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [],
+  },
+
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxt/ui",
+  ],
+});
